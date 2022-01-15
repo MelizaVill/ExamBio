658e66fc9441e82f142e6632959666b8ec2c6bc4
MelizaVill <94480230+MelizaVill@users.noreply.github.com>
2022-01-15 04:40:20 -0500

    video5

---
+++ ejercicio5_1103.sh
@@ -0,0 +1,19 @@
+cd ../../unix/data
+
+cd  Saavedra2013
+echo "Número de filas-polinizadores"
+cat ../Saavedra2013/n10.txt | wc -l
+echo "Número de columnas-plantas"
+head -n 1 ../Saavedra2013/n10.txt | tr -d " " | tr -d "\n" | wc -c
+cd ../Saavedra2013
+
+echo "Todas las filas"
+for f in ../Saavedra2013/*.txt;
+do
+	cat $f  | wc -l;
+done
+echo "Totas las columnas"
+for f in ../Saavedra2013/*.txt;
+do
+        head -n 1 $f | tr -d " " | tr -d "\n" | wc -c;
+done
